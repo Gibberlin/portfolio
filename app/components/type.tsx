3fa6933cb7967617c1d3e4309209a4ec5a2be83e
Gibberlin <syedeyashin@gmail.com>
2025-04-14 14:17:05 +0530

    scaled the type and navbar

--- app/components/type.tsx
+++ app/components/type.tsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Type() {
     return (
-        <div className="flex flex-row items-center justify-center m-2 w-full">
+        <div className="flex flex-row items-center justify-center text-center m-2 w-full">
             <TypeAnimation
                 sequence={[
                     "Hello, I'm Yashin",
@@ -19,7 +19,7 @@ export default function Type() {
                     700,
                 ]}
                 repeat={1}
-                className="text-7xl font-bold text-green-500"
+                className="md:text-7xl text-3xl font-bold text-green-500"
             />
         </div>
     )
